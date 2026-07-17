8a59657ed7eb1e954910690e71a43346041003b3
Afdhalhd12 <afdhalhadisolahudin@gmail.com>
2026-07-17 14:33:50 +0700

    refactor : php to js

--- application/views/denda/update.php
+++ application/views/denda/update.php
@@ -1,6 +1,5 @@
 <a href="<?= base_url('pustakawan/dashboard') ?>">Kembali</a>
 <h3>Edit Nominal Denda</h3>
-<<<<<<< HEAD
 
 <div class="form-container">
 	<form id="formDenda" method="post">
@@ -42,17 +41,3 @@
 	});
 
 </script>
-=======
-	
-    <div class="form-container">
-        <form action="<?= base_url('denda/proses_update') ?>" method="post">
-            <input type="hidden" name="denda_id" value="<?= $denda->id ?>">
-            
-            <label>Nominal Denda (Rp):</label>
-            <input type="number" name="nominal" value="<?= $denda->nominal ?>" required>
-            
-            <button type="submit">Update Nominal</button>
-            <a href="<?= base_url('peminjaman') ?>" class="btn-batal">Batal</a>
-        </form>
-    </div>
->>>>>>> b7385b754d74cac4e74aa21d0125ca72526bad1a
